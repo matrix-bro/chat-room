21c1debde23071e22064ad7c4ab3a6a8c74ab5c6
matrix-bro <razmhz11@gmail.com>
2023-12-31 11:53:30 +0545

    Log in functionality done

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index-page'),
     path('signup/', views.signup, name='signup-page'),
+    path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login-page'),
 ]
